edc557d2ecbba3c29df72a5bd4a1113009dc399b
Vidhi Parmar <parmaravidhi@gmail.com>
2024-02-24 14:35:01 +0530

    committed

--- client/src/Pages/UserAuthForm.jsx
+++ client/src/Pages/UserAuthForm.jsx
@@ -50,7 +50,9 @@ const UserAuthForm = ({ type }) => {
                 className="text-[#9f93da] text-xs hover:underline underline-offset-2 rounded-lg ml-2"
                 to={type === "login" ? "/register" : "/login"}
               >
-                Signup
+                {
+                type === "login" ? "Register" : "Login"
+              }
               </Link>
             </div>
 
@@ -64,58 +66,6 @@ const UserAuthForm = ({ type }) => {
       </div>
     </div>
   );
-  // } else if (type === "register") {
-  //   return (
-  //     <div className="flex flex-col md:flex-row h-[calc(100vh-70px)]">
-  //       <img
-  //         className="w-full md:w-[40vw] h-[40vh] md:h-full"
-  //         src={front}
-  //         alt="image"
-  //       />
-  //       <div className="flex w-full h-full justify-center items-center">
-  //         <form className="md:w-1/2 text-center">
-  //           <h1 className="text-3xl text-center font-bold">REGISTER</h1>
-  //           <div className="w-full h-full rounded-lg">
-  //             <input
-  //               placeholder="Name"
-  //               name="text"
-  //               type="text"
-  //               className="bg-[#e7e6e9] rounded-lg mt-4 w-full md:w-56 h-10 pl-3"
-  //             />
-  //             <br />
-  //             <input
-  //               placeholder="Email"
-  //               name="email"
-  //               type="email"
-  //               className="bg-[#e7e6e9] rounded-lg mt-4 w-full md:w-56 h-10 pl-3"
-  //             />
-  //             <br />
-  //             <input
-  //               type="password"
-  //               placeholder="Password"
-  //               name="password"
-  //               className="bg-[#e7e6e9] rounded-lg mt-4 w-full md:w-56 h-10 pl-3"
-  //             />
-  //             <br />
-  //             <a className="text-[#9f93da] text-xs hover:bg-gray-200 rounded-lg mr-2">
-  //               forgot password
-  //             </a>
-  //             <Link
-  //               className="text-[#9f93da] text-xs hover:bg-gray-200 rounded-lg ml-2"
-  //               to={"/login"}
-  //             >
-  //               Login
-  //             </Link>
-  //             <br />
-  //             <button className="h-10 w-full md:w-56 mt-4 text-white font-bold bg-[#7B68DB] rounded-lg align-middle text-center">
-  //               Register
-  //             </button>
-  //           </div>
-  //         </form>
-  //       </div>
-  //     </div>
-  // );
-  // }
 };
 
 export default UserAuthForm;
